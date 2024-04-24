9291bb90ef570776d5034b755d758b33b276561e
Joshua-Frazier <joshua.frazier1519@gmail.com>
2024-04-23 19:14:51 -0600

    adjust search bar

---
+++ src/components/Login/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>stuff is going on</h1>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -3,30 +3,47 @@ import logo from "../../assets/favicon.png"; // Adjust the path as necessary
 import "./Navbar.css";
 
 const Navbar = () => {
+  const onHandleLogin = () => {};
+
   return (
     <nav className="navbar bg-custom text-white outline">
-      <div className="container-fluid">
-        <a className="navbar-brand text-white" href="/">
-          <img
-            src={logo}
-            alt="Logo"
-            width="50"
-            height="50"
-            className="d-inline-block align-text-middle mx-2"
-          />
-          Robo Rave
-        </a>
-        <form className="d-flex" role="search">
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        <div className="d-flex align-items-center">
+          {" "}
+          {/* Logo at the far left */}
+          <a className="navbar-brand text-white" href="/">
+            <img
+              src={logo}
+              alt="Logo"
+              width="50"
+              height="50"
+              className="d-inline-block align-text-middle mx-2"
+            />
+            Robo Rave
+          </a>
+        </div>
+
+        <div className="search-container">
+          {" "}
           <input
-            className="form-control me-2"
+            className="form-control"
             type="search"
             placeholder="Search"
             aria-label="Search"
           />
-          <button className="btn btn-outline-success" type="submit">
-            Search
+        </div>
+
+        <div className="login-container d-flex justify-content-end">
+          {" "}
+          {/* Log In button at the far right */}
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={onHandleLogin}
+          >
+            Log In
           </button>
-        </form>
+        </div>
       </div>
     </nav>
   );
